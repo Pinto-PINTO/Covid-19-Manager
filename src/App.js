b9f56b493572c6d34e0789b40865737784c1338d
Menuka Pinto <instafeed9999@gmail.com>
2021-08-25 23:07:10 +0530

    Added Product Button with react-draggable

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Button from '@material-ui/core/Button';
 import SideMenu from "./components/SideMenu";
 import NavBar from './components/NavBar';
 import InStockSection from './components/InStockSection';
+import UploadButtons from './components/t'
 // import { makeStyles } from '@material-ui/styles';
 
 
@@ -14,8 +15,8 @@ function App() {
     <div className="App">      
       <NavBar/>
       <SideMenu/>
-      {/* <InStockSection/> */}
-      
+      <InStockSection/>
+      <UploadButtons/>
     </div>
   );
 }
--- src/components/InStockSection.js
+++ src/components/InStockSection.js
@@ -2,6 +2,9 @@ import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import Paper from '@material-ui/core/Paper';
 import Grid from '@material-ui/core/Grid';
+import { Container } from '@material-ui/core';
+import StockSearch from './StockSearch'
+import ProductAddBtn from './ProductAddBtn'
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -14,21 +17,27 @@ const useStyles = makeStyles((theme) => ({
   },
 }));
 
+
+
 export default function InStockSection() {
   const classes = useStyles();
 
   return (
     <div className={classes.root}>
-      <Grid container spacing={3}>
-        <Grid item xs>
-          <Paper className={classes.paper}>xs</Paper>
+      <Grid container>
+        <Grid item xs={6}>
+          <Paper className={classes.paper}>
+            <StockSearch/>
+          </Paper>
         </Grid>
-        <Grid item xs>
-          <Paper className={classes.paper}>xs</Paper>
+        <Grid item xs={6}>
+          <Paper className={classes.paper}>
+            <ProductAddBtn/>
+          </Paper>
         </Grid>
-        <Grid item xs>
+        {/* <Grid item xs>
           <Paper className={classes.paper}>xs</Paper>
-        </Grid>
+        </Grid> */}
       </Grid>
       {/* <Grid container spacing={3}>
         <Grid item xs>
