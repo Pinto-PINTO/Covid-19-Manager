6534ffc459d4cd2142b8a2623fcf7dcfab700240
Menuka Pinto <instafeed9999@gmail.com>
2021-08-25 22:12:03 +0530

    Navbar and SideMenu implemented

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import './App.css';
+import Button from '@material-ui/core/Button';
+import SideMenu from "./components/SideMenu";
+import NavBar from './components/NavBar';
+import InStockSection from './components/InStockSection';
+// import { makeStyles } from '@material-ui/styles';
+
+
+
+function App() {
+
+  return (
+    <div className="App">      
+      <NavBar/>
+      <SideMenu/>
+      {/* <InStockSection/> */}
+      
+    </div>
+  );
+}
+
+export default App;
