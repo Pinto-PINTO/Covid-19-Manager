d4363ca87bfca3a6c84702ab6e6c2ca29c524f03
Menuka Pinto <instafeed9999@gmail.com>
2021-09-18 12:04:48 +0530

    Allignments done inside the information modal form

--- src/components/Information/InformationModal.js
+++ src/components/Information/InformationModal.js
@@ -1,5 +1,6 @@
 import React from 'react'
-import { Button, Icon, Image, Modal } from 'semantic-ui-react'
+import { Button, Icon, Image, Modal} from 'semantic-ui-react'
+import { Grid, Container} from '@material-ui/core';
 
 import '../../App.css'
 
@@ -13,59 +14,92 @@ const InformationModal = () => {
             onOpen={() => setOpen(true)}
             trigger={<Button className="I_information_modal_btn">What to know more about the Inventory?</Button>}
         >
-            <Modal.Header>About Hospital Inventory</Modal.Header>
+            <Modal.Header className="I_information_modal_title" >About Hospital Inventory</Modal.Header>
 
             {/* ---------- Modal Content Inside START ---------- */}
 
             {/* ---------- Content 1 ---------- */}
-            <Modal.Content image scrolling>
-                <Image size='medium' src='https://react.semantic-ui.com/images/wireframe/image.png' wrapped />
+            <Modal.Content image scrolling className="I_information_modal_content">
+                <Container maxWidth="xl" className="containerPadding">
+                    <Grid container spacing={3} className="I_information_modal_grid_out">
 
-                <Modal.Description>
-                    <p>
-                        This is an example of expanded content that will cause the modal's
-                        dimmer to scroll.
-                    </p>
+                        <Grid item xs={12} sm={6} md={4} className="I_information_modal_grid_left">
+                            <Image size='medium' className="I_information_modal_img" src='/inventory_imgs/p1.jpg' wrapped />
+                        </Grid>
 
-                    <Image
-                        src='https://react.semantic-ui.com/images/wireframe/paragraph.png'
-                        style={{ marginBottom: 10 }}
-                    />
-                </Modal.Description>
+                        <Grid item xs={12} sm={6} md={8} className="I_information_modal_grid_right">
+                            <Modal.Description>
+                                <p>
+                                    This is an example of expanded content that will cause the modal's
+                                    dimmer to scroll.
+                                    This is an example of expanded content that will cause the modal's
+                                    dimmer to scroll.
+                                    This is an example of expanded content that will cause the modal's
+                                    dimmer to scroll.
+                                    This is an example of expanded content that will cause the modal's
+                                    dimmer to scroll.
+                                </p>
+                            </Modal.Description>
+                        </Grid>
+
+                    </Grid>
+                </Container>
             </Modal.Content>
 
             {/* ---------- Content 2 ---------- */}
-            <Modal.Content image scrolling>
-                <Image size='medium' src='https://react.semantic-ui.com/images/wireframe/image.png' wrapped />
+            <Modal.Content image scrolling className="I_information_modal_content">
+                <Container maxWidth="xl" className="containerPadding">
+                    <Grid container spacing={3} className="I_information_modal_grid_out">
 
-                <Modal.Description>
-                    <p>
-                        This is an example of expanded content that will cause the modal's
-                        dimmer to scroll.
-                    </p>
+                        <Grid item xs={12} sm={6} md={4} className="I_information_modal_grid_left">
+                            <Image size='medium' className="I_information_modal_img" src='/inventory_imgs/p1.jpg' wrapped />
+                        </Grid>
 
-                    <Image
-                        src='https://react.semantic-ui.com/images/wireframe/paragraph.png'
-                        style={{ marginBottom: 10 }}
-                    />
-                </Modal.Description>
+                        <Grid item xs={12} sm={6} md={8} className="I_information_modal_grid_right">
+                            <Modal.Description>
+                                <p>
+                                    This is an example of expanded content that will cause the modal's
+                                    dimmer to scroll.
+                                    This is an example of expanded content that will cause the modal's
+                                    dimmer to scroll.
+                                    This is an example of expanded content that will cause the modal's
+                                    dimmer to scroll.
+                                    This is an example of expanded content that will cause the modal's
+                                    dimmer to scroll.
+                                </p>
+                            </Modal.Description>
+                        </Grid>
+
+                    </Grid>
+                </Container>
             </Modal.Content>
 
             {/* ---------- Content 3 ---------- */}
-            <Modal.Content image scrolling>
-                <Image size='medium' src='https://react.semantic-ui.com/images/wireframe/image.png' wrapped />
+            <Modal.Content image scrolling className="I_information_modal_content">
+                <Container maxWidth="xl" className="containerPadding">
+                    <Grid container spacing={3} className="I_information_modal_grid_out">
 
-                <Modal.Description>
-                    <p>
-                        This is an example of expanded content that will cause the modal's
-                        dimmer to scroll.
-                    </p>
+                        <Grid item xs={12} sm={6} md={4} className="I_information_modal_grid_left">
+                            <Image size='medium' className="I_information_modal_img" src='/inventory_imgs/p1.jpg' wrapped />
+                        </Grid>
 
-                    <Image
-                        src='https://react.semantic-ui.com/images/wireframe/paragraph.png'
-                        style={{ marginBottom: 10 }}
-                    />
-                </Modal.Description>
+                        <Grid item xs={12} sm={6} md={8} className="I_information_modal_grid_right">
+                            <Modal.Description>
+                                <p>
+                                    This is an example of expanded content that will cause the modal's
+                                    dimmer to scroll.
+                                    This is an example of expanded content that will cause the modal's
+                                    dimmer to scroll.
+                                    This is an example of expanded content that will cause the modal's
+                                    dimmer to scroll.
+                                    This is an example of expanded content that will cause the modal's
+                                    dimmer to scroll.
+                                </p>
+                            </Modal.Description>
+                        </Grid>
+
+                    </Grid>
+                </Container>
             </Modal.Content>
 
             {/* ---------- Modal Content Inside END ---------- */}
