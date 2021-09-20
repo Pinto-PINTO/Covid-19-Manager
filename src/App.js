60a414576b8273a3157c538dcc7d96e3a32a21dd
Menuka Pinto <instafeed9999@gmail.com>
2021-09-20 11:15:43 +0530

    Added React-Router-Dom

--- src/App.js
+++ src/App.js
@@ -2,14 +2,20 @@ import React from 'react';
 import './App.css';
 
 import InventoryDashboard from './components/InventoryDashboard/InventoryDashboard'
+import I_Functions from './components/Functions/I_Functions'
+
+
+
 
 function App() {
 
   return (
     <div className="App">
 
+      {/* <I_Functions /> */}
       <InventoryDashboard />
-      
+
+
     </div>
   );
 }
